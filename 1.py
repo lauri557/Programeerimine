8ab4f95ad1f5cb5adcd85ec7001ca95c996e78ea
Lauri Lindmets <laurikas557@hotmail.com>
2013-10-31 13:42:38 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-print "Tere"
+eesnimi = raw_input('Mis su nimi on:')
+perekonnanimi = raw_input('Mis su perekonnanimi on:')
+
+if eesnimi == "Jaan-Edgar" and perekonnanimi == 'Urm':
+	print "Sina, v2ike v22nik, oled mulle v6lgu 15 Eurot."
+else:
+	print "Sinu nimi on " + eesnimi + " " + perekonnanimi
+
+vastus = raw_input('Kas sa maksad 2ra enne 5. Nov?:')
+
+if vastus == "Jah" or "ja" or "jh":
+	print "Okey, tore seda kuulda"
+else:
+	print "Killin sinu 2ra kui ei maksa"
